8d3ea5782e1619a4788d9f292fbffed62561271d
Stevenzwzhai <zzwdzsw@sina.cn>
2017-04-25 19:21:01 +0800

    使用vue编写

---
+++ demo2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <DistrictSelect></DistrictSelect>
+    </div>
+</template>
+
+<script>
+
+    import DistrictSelect from './components/DistrictSelect'
+    import Vue from 'vue'
+    Vue.use(DistrictSelect)
+export default {
+    name: 'app',
+    data () {
+      return {
+          msg: 'Welcome to Your Vue.js App'
+      }
+   }
+}
+</script>
+
+<style>
+
+</style>
