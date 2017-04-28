c558ae199a848fb5ea39181320904c9f12a10ad4
Stevenzhai <Stevenzwzhai@users.noreply.github.com>
2017-04-28 09:35:50 +0800

    Update index.js

--- demo2/src/components/DistrictSelect/index.js
+++ demo2/src/components/DistrictSelect/index.js
@@ -1,7 +1,6 @@
 /**
  * Created by Administrator on 2017/4/25.
  */
-import Vue from 'vue'
 import DistrictSelect from './districtSelect.vue'
 
 DistrictSelect.install = (Vue) => Vue.component('DistrictSelect', DistrictSelect);
